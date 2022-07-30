29e4a92e3d6392202a6c05f56dfb7e6214f821d8
sezercik <szrakoraler@gmail.com>
2022-07-30 22:36:50 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager:IProductService
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,11 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+List<Product> products = productManager.GetAll();
+foreach (Product product in products)
+{
+ Console.WriteLine(product.ProductName);   
+}
