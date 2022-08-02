7e037c02dcaca548cfe020cecbeacd1b237d50d4
sezercik <szrakoraler@gmail.com>
2022-08-02 18:42:27 +0300

    Added results

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -19,6 +20,18 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
+
+        public IResult Add(Product product)
+        {
+            //business codes
+            if(product.ProductName.Length > 2)
+            {
+                return new ErrorResult("Ürün ismi min 2 karakter olmalıdır");
+            }
+            _productDal.Add(product);
+            return new Result(true,"Ürün Eklendi");
+        }
+
         public List<Product> GetAll()
         {
             //iş kodları
@@ -33,6 +46,11 @@ namespace Business.Concrete
         public List<Product> GetAllByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public Product GetById(int productID)
+        {
+            return _productDal.Get(p => p.ProductId == productID);
         }
 
         public List<ProductDetailDto> GetProductDetails()
