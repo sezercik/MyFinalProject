68dc73b8a522895284a9f894254e3ff960800f50
sezercik <szrakoraler@gmail.com>
2022-08-05 00:11:51 +0300

    Day:13 : AOP

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -17,21 +20,35 @@ using FluentValidation;
 
 namespace Business.Concrete
 {
-    public class ProductManager:IProductService
+    public class ProductManager : IProductService
     {
         private readonly IProductDal _productDal;
+        private ICategoryService _categoryService;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal,ICategoryService categoryService)
         {
-            _productDal = productDal;
+            _categoryService = categoryService;
         }
+
+
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            //business codes
+            //aynı isim de ürün eklenemez
+            //Eğer mevcut kategori sayısı 15'i geçtiyse sisteme yeni ürün eklenemez
+            IResult result = BusinessRules.Run(
+                 CheckIfProdcutCountOfCategoryCorrect(product.CategoryId),
+                 CheckIfProductNameExists(product.ProductName),
+                 CheckIfCategoryLimitExceded()
+                 );
+
+            if (result != null)
+            {
+                return result;
+            }
             _productDal.Add(product);
-            return new Result(true,Messages.ProductAdded);
+            return new Result(true, Messages.ProductAdded);
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -41,7 +58,7 @@ namespace Business.Concrete
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
@@ -53,7 +70,7 @@ namespace Business.Concrete
         {
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
-            
+
         public IDataResult<Product> GetById(int productID)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productID));
@@ -62,6 +79,41 @@ namespace Business.Concrete
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IResult CheckIfProdcutCountOfCategoryCorrect(int categoryId)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 15)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count > 15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+            return new SuccessResult();
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,41 +3,45 @@ using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 
+void Main()
+{
+    Console.WriteLine("test");
 
+}
 //SOLID
 //Open Closed principle
-ProductTest();
+//ProductTest();
 //CategoryTest();
 
 
-void ProductTest()
-{
- ProductManager productManager = new ProductManager(new EfProductDal());
+//void ProductTest()
+//{
+//    //ProductManager productManager = new ProductManager(new EfProductDal(),new CategoryManager(new EfProductDal()));
 
-    //List<Product> products = productManager.GetAll();
-    //foreach (Product product in products)
-    //{
-    // Console.WriteLine(product.ProductName);   
-    //}
-    var result = productManager.GetProductDetails();
-    if (result.Success)
-    {
-        foreach (var product in result.Data)
-        {
-            Console.WriteLine(product.ProductName + " / " + product.CategoryName);
-        }
-    }
-    else
-    {
-        Console.WriteLine(result.Message);
-    }
-}
+//    //List<Product> products = productManager.GetAll();
+//    //foreach (Product product in products)
+//    //{
+//    // Console.WriteLine(product.ProductName);   
+//    //}
+//    var result = productManager.GetProductDetails();
+//    if (result.Success)
+//    {
+//        foreach (var product in result.Data)
+//        {
+//            Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine(result.Message);
+//    }
+//}
 
-void CategoryTest()
-{
- CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
- foreach (Category category in categoryManager.GetAll())
- {
-  Console.WriteLine(category.CategoryName);
- }
-}
+//void CategoryTest()
+//{
+//    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+//    foreach (Category category in categoryManager.GetAll())
+//    {
+//        Console.WriteLine(category.CategoryName);
+//    }
+//}
